fbf6e8b0cb5ff940329540f57622511fb34cfd2b
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2026-06-27 00:40:53 +0900

    主に整数の標準入力を受け取る関数を追加した

--- Main.hs
+++ Main.hs
@@ -7,14 +7,22 @@ import Data.Bool
 
 main :: IO ()
 main = do
-    [x, y] <- map read . words <$> getLine :: IO [Int]
+    [x, y] <- inputInts
     let isEqual = 9 * x == 16 * y
-    printYn isEqual
+    printYesNo isEqual
+
+-- | \( O(n) \) : 1 行の 1 個の整数を受け取る
+inputInt :: IO Int
+inputInt = readLn
+
+-- | \( O(n) \) : 1 行の複数個の整数を受け取る
+inputInts :: IO [Int]
+inputInts = map read . words <$> getLine
 
 -- | \( O(1) \) : ブール値を @\"Yes\"@ / @\"No\"@ の形式に変換する
 yn :: Bool -> String
 yn = bool "No" "Yes"
 
 -- | \( O(1) \) : ブール値を @\"Yes\"@ / @\"No\"@ の形式で出力する
-printYn :: Bool -> IO ()
-printYn = putStrLn . yn
+printYesNo :: Bool -> IO ()
+printYesNo = putStrLn . yn
