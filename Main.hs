762fe485e58103da383d705f3ce8f33de1f0ea49
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2026-07-02 01:30:32 +0900

    入力を関数で受け取れるようにした

--- Main.hs
+++ Main.hs
@@ -3,21 +3,47 @@
 
 module Main where
 
+import Control.Monad (replicateM)
 import Data.Bool
 
 main :: IO ()
 main = do
-    [x, y] <- inputInts
-    let isEqual = 9 * x == 16 * y
-    printYesNo isEqual
+    a <- inputInt
+    [b, c] <- inputSplitInt
+    s <- inputString
+    putStrLn $ show (a + b + c) ++ " " ++ s
 
--- | \( O(n) \) : 1 行の 1 個の整数を受け取る
+-- | 1 行の整数を受け取る
 inputInt :: IO Int
 inputInt = readLn
 
--- | \( O(n) \) : 1 行の複数個の整数を受け取る
-inputInts :: IO [Int]
-inputInts = map read . words <$> getLine
+-- | 1 行の文字列を受け取る
+inputString :: IO String
+inputString = getLine
+
+-- | 1 行の空白区切りで与えられる整数を受け取る
+inputSplitInt :: IO [Int]
+inputSplitInt = map read . words <$> getLine
+
+-- | 1 行の空白区切りで与えられる文字列を受け取る
+inputSplitString :: IO [String]
+inputSplitString = words <$> getLine
+
+-- | 複数行の整数を受け取る
+inputInts :: Int -> IO [Int]
+inputInts lineCount = replicateM lineCount readLn
+
+-- | 複数行の文字列を受け取る
+inputStrings :: Int -> IO [String]
+inputStrings lineCount = replicateM lineCount getLine
+
+-- | 複数行の空白区切りで与えられる整数を受け取る
+inputSplitInts :: Int -> IO [[Int]]
+inputSplitInts lineCount = replicateM lineCount inputSplitInt
+
+-- | 複数行の空白区切りで与えられる文字列を受け取る
+inputSplitStrings :: Int -> IO [[String]]
+inputSplitStrings lineCount = replicateM lineCount inputSplitString
 
 -- | \( O(1) \) : ブール値を @\"Yes\"@ / @\"No\"@ の形式に変換する
 yn :: Bool -> String
