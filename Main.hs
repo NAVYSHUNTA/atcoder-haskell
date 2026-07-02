25b1b5ff081b3c2f52088720f0477614aeffcb46
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2026-07-03 01:09:08 +0900

    複雑な出力を簡潔な関数で行えるようにした

--- Main.hs
+++ Main.hs
@@ -45,6 +45,22 @@ inputSplitInts lineCount = replicateM lineCount inputSplitInt
 inputSplitStrings :: Int -> IO [[String]]
 inputSplitStrings lineCount = replicateM lineCount inputSplitString
 
+-- | 整数を空白区切りで出力する
+outputSplitInt :: [Int] -> IO ()
+outputSplitInt output = putStrLn $ unwords . map show $ output
+
+-- | 文字列を空白区切りで出力する
+outputSplitString :: [String] -> IO ()
+outputSplitString output = putStrLn $ unwords output
+
+-- | 整数を改行区切りで出力する
+outputLineInt :: [Int] -> IO ()
+outputLineInt output = putStr $ unlines . map show $ output
+
+-- | 文字列を改行区切りで出力する
+outputLineString :: [String] -> IO ()
+outputLineString output = putStr $ unlines output
+
 -- | \( O(1) \) : ブール値を @\"Yes\"@ / @\"No\"@ の形式に変換する
 yn :: Bool -> String
 yn = bool "No" "Yes"
