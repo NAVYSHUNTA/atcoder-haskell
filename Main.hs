14f06aa46d59a3fed4f012a2049a1d361696ef39
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2026-07-05 01:34:53 +0900

    高速な IO を用意した

--- Main.hs
+++ Main.hs
@@ -5,37 +5,51 @@ module Main where
 
 import Control.Monad
 import Data.Bool
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
 
 main :: IO ()
 main = do
     a <- inputInt
     [b, c] <- inputSplitInt
     s <- inputString
-    putStrLn $ show (a + b + c) ++ " " ++ s
+    putStrLn $ show (a + b + c) <> " " <> s
 
 -- | 1 行の整数を受け取る
 inputInt :: IO Int
-inputInt = readLn
+inputInt = fst . fromJust . BS.readInt <$> BS.getLine
 
 -- | 1 行の文字列を受け取る
 inputString :: IO String
 inputString = getLine
 
+-- | 1 行の文字列を受け取る
+inputByteString :: IO BS.ByteString
+inputByteString = BS.getLine
+
 -- | 1 行の空白区切りで与えられる整数を受け取る
 inputSplitInt :: IO [Int]
-inputSplitInt = map read . words <$> getLine
+inputSplitInt = map (fst . fromJust . BS.readInt) . BS.words <$> BS.getLine
 
 -- | 1 行の空白区切りで与えられる文字列を受け取る
 inputSplitString :: IO [String]
 inputSplitString = words <$> getLine
 
+-- | 1 行の空白区切りで与えられる文字列を受け取る
+inputSplitByteString :: IO [BS.ByteString]
+inputSplitByteString = BS.words <$> BS.getLine
+
 -- | 複数行の整数を受け取る
 inputInts :: Int -> IO [Int]
-inputInts lineCount = replicateM lineCount readLn
+inputInts lineCount = replicateM lineCount inputInt
 
 -- | 複数行の文字列を受け取る
 inputStrings :: Int -> IO [String]
-inputStrings lineCount = replicateM lineCount getLine
+inputStrings lineCount = replicateM lineCount inputString
+
+-- | 複数行の文字列を受け取る
+inputByteStrings :: Int -> IO [BS.ByteString]
+inputByteStrings lineCount = replicateM lineCount inputByteString
 
 -- | 複数行の空白区切りで与えられる整数を受け取る
 inputSplitInts :: Int -> IO [[Int]]
@@ -45,6 +59,10 @@ inputSplitInts lineCount = replicateM lineCount inputSplitInt
 inputSplitStrings :: Int -> IO [[String]]
 inputSplitStrings lineCount = replicateM lineCount inputSplitString
 
+-- | 複数行の空白区切りで与えられる文字列を受け取る
+inputSplitByteStrings :: Int -> IO [[BS.ByteString]]
+inputSplitByteStrings lineCount = replicateM lineCount inputSplitByteString
+
 -- | 整数を空白区切りで出力する
 outputSplitInt :: [Int] -> IO ()
 outputSplitInt output = putStrLn $ unwords . map show $ output
@@ -53,6 +71,10 @@ outputSplitInt output = putStrLn $ unwords . map show $ output
 outputSplitString :: [String] -> IO ()
 outputSplitString output = putStrLn $ unwords output
 
+-- | 文字列を空白区切りで出力する
+outputSplitByteString :: [BS.ByteString] -> IO ()
+outputSplitByteString output = BS.putStrLn $ BS.unwords output
+
 -- | 整数を改行区切りで出力する
 outputLineInt :: [Int] -> IO ()
 outputLineInt output = putStr $ unlines . map show $ output
@@ -60,6 +82,10 @@ outputLineInt output = putStr $ unlines . map show $ output
 -- | 文字列を改行区切りで出力する
 outputLineString :: [String] -> IO ()
 outputLineString output = putStr $ unlines output
+
+-- | 文字列を改行区切りで出力する
+outputLineByteString :: [BS.ByteString] -> IO ()
+outputLineByteString output = BS.putStr $ BS.unlines output
 
 -- | \( O(1) \) : ブール値を @\"Yes\"@ / @\"No\"@ の形式に変換する
 yn :: Bool -> String
